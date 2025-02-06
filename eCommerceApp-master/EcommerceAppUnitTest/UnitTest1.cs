0eaf03bbdb46949ba2a52fa041e1db0fe5796968
Valentine <val@gmail.com>
2025-02-06 15:02:06 -0500

    created UnitTest1.cs

---
+++ eCommerceApp-master/EcommerceAppUnitTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+using eCommerceApp;
+namespace EcommerceAppUnitTest
+{
+    public class Tests
+    {
+        private Product _product;
+
+        [SetUp]
+        public void Setup()
+        {
+            _product = new Product(101, "Laptop", 999.99, 10, 0, 0);
+        }
+        
+
+        
+
+        
+       
+       
+
+
+    }
+}
