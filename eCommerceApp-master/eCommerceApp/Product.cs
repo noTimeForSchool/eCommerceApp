b8dbde23a4f4aa013750b669465b0c70c9c14fa0
Valentine <val@gmail.com>
2025-02-06 15:01:08 -0500

    created product.cs

--- eCommerceApp-master/eCommerceApp/Product.cs
+++ eCommerceApp-master/eCommerceApp/Product.cs
@@ -41,83 +41,20 @@ namespace eCommerceApp
             }
         }
 
-        public double ItemPrice
-        {
-            get
-            {
-                return _itemPrice;
-            }
-            set
-            {
-                if (value < 5 || value > 5000)
-                {
-                    throw new ArgumentOutOfRangeException(nameof(ItemPrice), "ItemPrice must be between 5 and 5000.");
-                }
-                _itemPrice = value;
-            }
-        }
-        public int StockAmount
-        {
-            get
-            {
-                return _stockAmount;
-            }
-            set
-            {
-                if (value < 5 || value > 500000)
-                {
-                    throw new ArgumentOutOfRangeException(nameof(StockAmount), "StockAmount must be between 5 and 500000.");
-                }
-                _stockAmount = value;
-            }
-        }
+        
 
-        public int StockIncrease
-        {
-            get
-            {
-                return _stockIncreaseAmount;
-            }
-            set
-            {
-                if ((value + _stockAmount) > 500000)
-                {
-                    throw new ArgumentOutOfRangeException(nameof(StockIncrease), "Stock amount with the Increase must not exceed 500000");
-                }
-                
-                _stockIncreaseAmount = value + _stockAmount;
-                _stockAmount = _stockIncreaseAmount;
-            }
-        }
-
-        public int StockDecrease
-        {
-            get
-            {
-                return _stockDecreaseAmount;
-            }
-            set
-            {
-                if ((_stockAmount - value) < 5)
-                {
-                    throw new ArgumentOutOfRangeException(nameof(StockDecrease), "Stock amount with the Decrease must not be less than 5");
-                }
-
-                _stockDecreaseAmount = _stockAmount - value;
-                _stockAmount = _stockDecreaseAmount;
-            }
-        }
+        
 
 
         public Product(int prodID, string prodName, double itemPrice, int stockAmount, int stockIncrease, int stockDecrease)
         {
             ProdID = prodID;
             ProdName = prodName;
-            ItemPrice = itemPrice;
-            StockAmount = stockAmount;
+            //ItemPrice = itemPrice;
+            //StockAmount = stockAmount;
 
-            StockIncrease = stockIncrease;
-            StockDecrease = stockDecrease;
+            //StockIncrease = stockIncrease;
+            //StockDecrease = stockDecrease;
         }
 
 
